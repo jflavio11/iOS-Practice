a65ab699f83052c3963519cd2a7980f84cbd39b1
Jose Flavio <jflavio90@gmail.com>
2021-03-20 11:16:10 -0500

    add peru landmarks project

---
+++ PeruLandmarksList/PeruLandmarksList/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jose Flavio Quispe on 20/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
