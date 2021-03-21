642925ef92cc91792914f883d207b0d7f04112b5
Jose Flavio <jflavio90@gmail.com>
2021-03-21 12:56:11 -0500

    custom view (badge) drawing paths and shapes

---
+++ PeruLandmarksList/View/CustomViews/Badge/BadgeBackground.swift
@@ -0,0 +1,77 @@
+//
+//  BadgeBackground.swift
+//  Peru Landmarks List
+//
+//  Created by Jose Flavio Quispe on 21/03/21.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        
+        // to increase view as container
+        GeometryReader { geometry in
+            
+            Path { path in
+                
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                
+                width *= xScale
+                
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95 + xOffset,
+                        y: height * (0.20 + HexagonParameters.adjustment)
+                    )
+                )
+                
+                HexagonParameters.segmens.forEach { segment in
+                    // The addLine(to:) method takes a single point and draws
+                    // it. Successive calls to addLine(to:) begin a line at
+                    // the previous point and continue to the new point.
+                    path.addLine(
+                        to: CGPoint(
+                            x: width * segment.line.x + xOffset,
+                            y: height * segment.line.y
+                        )
+                    )
+                    
+                    // Use the addQuadCurve(to:control:) method to draw the
+                    // Bézier curves for the badge’s corners.
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x + xOffset,
+                            y: height * segment.curve.y
+                        ),
+                        control: CGPoint(
+                            x: width * segment.control.x + xOffset,
+                            y: height * segment.control.y
+                        )
+                    )
+                }
+                
+            }.fill(
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.red, Color.blue]),
+                    startPoint: UnitPoint(x: 0.3, y: 0.68),
+                    endPoint: UnitPoint(x: 0.9, y: 0.1)
+                )
+            )
+            
+        }.aspectRatio(1, contentMode: .fit)
+        
+    }
+    
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+        
+    }
+}
---
+++ PeruLandmarksList/View/CustomViews/Badge/HexagonParameters.swift
@@ -0,0 +1,60 @@
+//
+//  HexagonParameters.swift
+//  Peru Landmarks List
+//
+//  Created by Jose Flavio Quispe on 21/03/21.
+//
+
+import Foundation
+import CoreGraphics
+
+struct HexagonParameters {
+    
+    // Actually defines a Point
+    // Each side starts where the previous ends, moves in a straight line
+    // to the first point, and then moves over a Bézier curve at the corner
+    // to the second point. The third point controls the shape of the curve.
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+    
+    static let adjustment: CGFloat = 0.085
+    
+    // defines the segments to draw an hexagon
+    static let segmens: [Segment] = [
+        
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
+            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
+            control: CGPoint(x: 0.00, y: 0.25 + adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
+            curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),
+            control: CGPoint(x: 1.00, y: 0.25 + adjustment)
+        )
+        
+    ]
+}
